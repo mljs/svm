537cecd99817e80340833708ca67e96f7b115058
Daniel Kostro <kostro.d@gmail.com>
2016-07-26 16:38:00 +0200

    add simple example

---
+++ example/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var SVM = require('..');
+
+var options = {
+    kernel: 'linear',
+    C:1,
+};
+
+var svm =  new SVM(options);
+
+const features = [[0,2],[0,0],[2,2],[2,0]];
+var labels = [-1, -1, 1, 1];
+
+// var features = [[0,0],[0,1],[1,1],[1,0]];
+// var labels = [1, -1, 1, -1];
+
+svm.train(features, labels);
+console.log(svm.X);
+
+console.log(svm.predict(features));
